be3b6e96672cdd400f90207c7e0748d6ed6d5b28
Shinnosuke1213 <Shinnosuke.K1213@gmail.com>
2024-11-14 22:36:04 +0900

    add supabase auth.
    add supabase auth, signin and signup. smtp server is gmail. (configure in supabase auth.)

---
+++ front/src/app/login/actions.ts
@@ -0,0 +1,55 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/app/utils/supabase/server'
+
+export async function login(formData: FormData) {
+  const supabase = await createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  if (error) {
+    console.log(error)
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  //if email domain is invalid.
+  const email_domain = data.email.substring(data.email.lastIndexOf("@") + 1);
+  if (email_domain != "oita.kosen-ac.jp") {
+    console.log("[AUTH] domain is invalid.")
+    redirect('/error')
+  }
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    console.log(error)
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/signup')
+}
